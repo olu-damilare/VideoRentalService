0f1ca770c653832f783eaf84a658fc25d893378a
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-07 05:44:47 +0100

    model mapper

---
+++ src/main/java/com/video/videorental/configurations/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.video.videorental.configurations;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
