78eea94097e56747675a1d93990c332134023fb1
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-07 05:46:05 +0100

    insert into db script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- select @myInt = COUNT(*) from video;
+
+INSERT INTO video(video_id, title, video_type, genre, rate)
+VALUES(11, 'Spectre', 'New Release: 2010', 'Action', 15),
+      (12, 'Krishna', 'Children''s Movie : 18', 'Drama', 8),
+      (13, 'Titanic', 'Regular', 'Romance', 10),
+      (14, 'Evil Dead', 'New Release: 2015', 'Horror', 15),
+      (15, 'Jackie', 'Children''s Movie : 18', 'Comedy', 15);
+
